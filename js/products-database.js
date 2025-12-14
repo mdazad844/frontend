50b69296023596d23e69c29e974601c3021460a5
mdazad844 <105972300+mdazad844@users.noreply.github.com>
2025-12-14 19:15:00 +0530

    Update products-database.js

--- js/products-database.js
+++ js/products-database.js
@@ -92,7 +92,7 @@ const productDatabase = {
     '5': {
         id: 5,
         name: 'Premium Black Polo T-shirt',
-        price: 289,
+        price: 1,
         images: ['product5.jpg', 'product5-back.jpg', 'product5-detail.jpg', 'product5-side.jpg'],
         description: 'Premium cotton  polo T-shirt with excellent breathability and professional look.',
         details: {
